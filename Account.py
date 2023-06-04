020b3608844218761e332189b35d1e8f0e9dadfb
Hydra-Bolt <ahmed075357@cloud.neduet.edu.pk>
2023-06-04 15:44:20 +0500

    modified:   Account.py
    Changes not staged for commit:
    	modified:   DesignClass.py
    	modified:   __pycache__/DesignClass.cpython-310.pyc
    	modified:   __pycache__/create_account.cpython-310.pyc
    	modified:   __pycache__/intro_bank.cpython-310.pyc
    	modified:   __pycache__/main.cpython-310.pyc
    	modified:   __pycache__/ui_functions.cpython-310.pyc
    	modified:   __pycache__/ui_main.cpython-310.pyc
    	modified:   bank.py
    	modified:   create_account.py
    	modified:   main.py
    	modified:   pymonog.py
    	deleted:    test.py
    	modified:   titlepage.py
    	modified:   ui_functions.py
    	modified:   ui_main.py
    
    Untracked files:
    	__pycache__/loanApp.cpython-310.pyc
    	__pycache__/loan_window.cpython-310.pyc
    	loanApp.py
    	loan_window.py
    	loanwindow.py

--- Account.py
+++ Account.py
@@ -1,13 +1,14 @@
 import random
 
 class Account:
-    def __init__(self,fullname,cnic,account_name,account_type,password_create) -> None:
+    def __init__(self, balance_label, money_label, deposit_label, withdraw_label, transfer_label, ) -> None: 
         self.fullname =fullname
         self.cnic = cnic
         self.account_name = account_name
         self.account_type =account_type
         self.password_create = password_create
-        self.account_number = "ETR-" + "".join([str(random.randint(0, 9)) for _ in range(6)]) + "-" + "".join([str(random.randint(0, 9)) for _ in range(6)])
+        self.account_number = account
         self.balance = 0
-    def getDocumentDB(self):
-        
+    def deposit(self):
+        self.balance+=int(self.deposit_label.text())
+        return self.balance
